5ad5935c5d184675f44db489add0af605e85880e
abijithvignesh <60066300+abijithvignesh@users.noreply.github.com>
2021-05-14 18:45:05 +0530

    Initial Setup and Landing page
    First Commit.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Navigation from './Components/Navigation'
+import HeroSection from './Components/HeroSection'
+import MiddleSection from './Components/MiddleSection'
+
+function App() {
+  return (
+    <>
+    <Navigation></Navigation>
+    <HeroSection></HeroSection>
+    <MiddleSection></MiddleSection>
+    
+    </>
+  );
+}
+
+export default App;
